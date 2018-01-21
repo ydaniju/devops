7480631ff7b2ebff9ca9630300e46eaad521fcf2
Yusuf Daniju <yusuf.daniju@andela.com>
2018-01-21 14:32:39 +0100

    add apt file

--- heroku.js
+++ heroku.js
@@ -4,4 +4,16 @@ var heroin = require('heroin-js');
 
 var configurator = heroin(process.env.HEROKU_API_TOKEN);
 
-configurator({name: 'pruner'});
+config = {
+  name: 'pruner',
+  buildpacks: [
+    'https://github.com/heroku/heroku-buildpack-ruby',
+    'https://github.com/heroku/heroku-buildpack-apt'
+  ]
+}
+
+configurator(config);
+
+configurator.export('pruner').then(function (result) {
+  console.log(result);
+});
